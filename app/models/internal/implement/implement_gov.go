35b3e7449e11d22465cdadbd24754cd0b2ae6e0f
York-xia <1208209000@qq.com>
2022-03-27 13:43:02 +0800

    fixcode

--- app/models/internal/implement/implement_gov.go
+++ app/models/internal/implement/implement_gov.go
@@ -18,7 +18,7 @@ type ImplementGov struct {
 	ConstructSite           string          `gorm:"column:construct_site;type:varchar(200);comment:建设地点"`
 	ProjectType             *int            `gorm:"column:project_type;type:integer;comment:项目类型 0:安置房,1:道路交通,2:市政设施;3:提升整治;4:卫生;5:五水共治;6:学校;7:其他"`
 	PlanBegin               *time.Time      `gorm:"column:plan_begin;type:timestamp;comment:计划开工时间"`
-	BeginImpl               *time.Time      `gorm:"column:plan_begin;type:timestamp;comment:开工时间"`
+	BeginImpl               *time.Time      `gorm:"column:begin_impl;type:timestamp;comment:开工时间"`
 	Period                  *int            `gorm:"column:period;type:integer;comment:建设周期"`
 	PointType               *int            `gorm:"column:point_type;type:integer;comment:重点类型 0:省重点实施项目,1:省重点预备项目,2:省重大产业项目;3:省4+1项目;4:省6千亿项目;5:市重点实施项目;6:市重点预备项目;7:无重点类型;8: 152工程"`
 	ImplementType           *int            `gorm:"column:implement_type;type:integer;comment:实施类型 0:新开工,1:续建"`
